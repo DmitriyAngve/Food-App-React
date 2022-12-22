7e1fc3607fe778b7caadd455520dca3eda39edd3
Dmitriy Angve <kirirch129@gmail.com>
2022-12-22 08:57:44 +0300

    #

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,10 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState();
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-project-angve-3-default-rtdb.firebaseio.com/meals.json"
+      );
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
@@ -31,5 +58,31 @@ export default AvailableMeals;
 // 1.2 Delete "DUMMY_MEALS" array.
 // 1.3 Let's change the code: add "fetch()".
 // 1.4 need to fetch meals data from data-base, when "AvailableMeals" Component is loaded. I can utilize the "useEffect" hook for this.
-// 1.4.1 Import "useEffect()"
+// 1.4.1 Import "useEffect()" from React to trigger a side effect like fetching data from the web, when some data changes (or, as a mention above, when that Component is loaded for the first time).
+// 1.4.2 Inside "useEffect" we put "fetch()" function (1.1)
+// 1.4.2 In "fetch()" we wanna send an HTTP request to meals from data-base. Default "GET" request.
+// Fetch returns a promise - dont forget! And HTTP request is a asynchronous task. I can add "then()" after request tp provide a function: "....then(() => {})", which is triggered when the request is done (received from Firebase), once we got a response. Or I can use async await.
+// The function you pass to "useEffect" should not return a promise. That the function you pass to "useEffect" may return a cleanup function which can be executed. That cleanup function should run senchronously though. It should not retun a promise or anything like this. Overall function which we pass to "useEffect" must not be turned into a sync function. Instead, if I wanna use async await in the "useEffect" simply create a new function: "fetchMeals" and execute "fetchmeals" as a part of "useEffect". This function is still executed and I can still use "async-await". USEEFFECT DOES NOW NOT RETURN A PROMISE. It's now not an async function! because we have not added async.
+// 1.5 Get a "response" for fetching our meals. Store fetching to const.
+// 1.6 Now we can parse data: "responseData" by calling "const responseData = response.json()", which also returns a promise.
+// Now we can utilize "responseData". Firebase specific: will always be an object where these ids (m1-m4) will be keys, and then the values for those keys will be nested objects with properties (look at Firebase).
+// "responseData" will be an objects, and I want an array. Let's transorm this.
+// 1.7 For transform we create a new constant "const loadedMeals = []" which is an empty array, and then I use a for/in loop to go through al lthe keys in "responseData" object (keys will be ids from Firebase and values will then be the nested objects)
+// 1.8 Then wanna reach out to "loadedMeals" and "push" a new object into this initially empty array where we set the id property. We need to set id here, because we do expect our meals to have an id field ("id={meal.id}"). So we should make sure that we transform the loaded data such that we do have an id field. The "key" will be the "id" (from Firebase) of the individual meals we-re fetching.
+// 1.9 "name: responseData" name can be pulled out of the "responseData" for the given "key" with that, we access the nested object in there and then that nested object will have a name field: "name: responseData[key].name,".
+// 1.10 "description" the same.
+// 1.11 (1.7 - 1.10) That was transforming the fetch data.
+// Need to expose the fetching data to the rest of our Component, and we wanna re-render the Component once the fetching is done, because that's an asyncchronous task, which is only started after the Component loaded for the first time and initially, there will be no data and we wanna update the Component once the  data is there. Now, when we have data that changes and where a Component should be re-evaluated once it did change, whanever we have a use case like this, we need state.
+// STEP: 2 (useState)
+// 2.1 Import "useState".
+// 2.2 Set up some state in this Component. In this case initially state will be an empty array because this state will be our meals (from data-base) and "setMeals" state-updating function. It's now the meals which I wanna use down -> instead of "DUMMY_MEALS" to render "MealsItems". "const mealsList = meals.map((meal) => (..." and initially, that's an empty array but that will change once the loaded data.
+// After "for-loop" once we filled our "loadedMeals" helper constant with the transformed data, I wanna call "setMeals".
+// 2.3 So I wanna call that state-updating function and pass in the "loadedMeals". So that array which we populated. With that, we're setting the data, and therefore then this Component ("MealItem") should update.
+// For this "useEffect" dependency array is an empty array. We have no dependencies, which means this "useEffect" function will only run when the Component was first loaded. It will never run again thereafter.
 // ~~ FETCHING MEALS VIA HTTP ~~
+
+// ~~ HANDLING THE LOADING STATE ~~
+// Now we add loading and error state. To achieve this, we need to manage more our state (useReducer or several useState's)
+// STEP: 1
+// 1.1 Add new state: "const [isLoading, setIsLoading] = useState()"
+// ~~ HANDLING THE LOADING STATE ~~
