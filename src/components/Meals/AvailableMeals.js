1ca6564c71071081276c72179e67999296647f1d
Dmitriy Angve <kirirch129@gmail.com>
2022-12-22 11:23:28 +0300

    #

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,13 +5,22 @@ import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
-  const [isLoading, setIsLoading] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
+      // setIsLoading(true); // in case if useState(false)
+
       const response = await fetch(
-        "https://react-project-angve-3-default-rtdb.firebaseio.com/meals.json"
+        // "https://react-project-angve-3-default-rtdb.firebaseio.com/meals.json"
+        "https://react-project-angve-3-default-rtdb.firebaseio.com/meals.json" // simulate the error
       );
+
+      if (!response.ok) {
+        throw new Error("Something wen wrong!");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -26,10 +35,26 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return <section className={classes.MealsError}>{httpError}</section>;
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
@@ -81,8 +106,42 @@ export default AvailableMeals;
 // For this "useEffect" dependency array is an empty array. We have no dependencies, which means this "useEffect" function will only run when the Component was first loaded. It will never run again thereafter.
 // ~~ FETCHING MEALS VIA HTTP ~~
 
+//
+
 // ~~ HANDLING THE LOADING STATE ~~
 // Now we add loading and error state. To achieve this, we need to manage more our state (useReducer or several useState's)
 // STEP: 1
-// 1.1 Add new state: "const [isLoading, setIsLoading] = useState()"
+// 1.1 Add new state: "const [isLoading, setIsLoading] = useState(false)" and initially we're not loading (false).
+// 1.2 Set initial state to true. Because we know that we always will start with loading data in this Component when the Component is rendered. If a have a Component where that's not necessarily the case, where you're not necessarily loading just because the Component was created for the first time, there you might not want to set this to "true"
+// 1.3 After "setMeals(loadedMeals)" add "setIsLoading(false)" because we now definitel are done loading. And nowwe can use "setIsLoading" to control what we render
+// We could say, that we don't wanna render our meals at all. So "<section className={classes.meals}..." this entire section should not be rendered at all if we're loading. I can check, if "isLoading" is true.
+// STEP: 2
+// 2.1 Add "ifcheck" check if "isLoading" is true. And if it true, I will return: "if (isLoading) {return (<section></section>..."
+// 2.2 Let's stylled by adding a "<section className={classes.MealsLoading}>"
+// 2.3 in CSS add a "MealsLoading"
 // ~~ HANDLING THE LOADING STATE ~~
+
+//
+
+// ~~ HANDLING THE ERROR ~~
+// STEP: 1
+// For handling the error we need a third state that determines whether we have an error or not.
+// 1.1 Add "const [httpError, setHttpError] = useState(false)" and initially set to null. (may be undefined or false - one of falsey). Set it to null to be more explicit about my goal of not having any value initially.
+// 1.2 Set error. For this a wanna add "ifcheck" my "response" okay is falsey. ("if (!response.ok) {...}") If it's not ok!
+// If we have some error status code reported back by the server. If that should be the case I wanna "throw" a new error here where I say "Something wen wrong". We could also look into the error response and see if the server gave us a more useful error string.
+// 1.3 Go to the place where we call "fetchMeals", which is still inside of "useEffect()" but outside of the "fetchMeals" function and wrap this with try-catch. So we try calling "fetchMeals", but if inside of that function an error is thrown, as I wrote above, then we make it into this catch block, which is still inside the effect function but outside of the "fetchMeals" function.
+// 1.4 For "catch" I wanna set "setIsLoading(false)" to false because we're not loading anymore. Even if it's an erro, we're done loading. And then I wanna "setHttpError" to the error we caught. '...}catch (error) { setIsLoading(false);setHttpError(error.message);}. So we do get access to that error here when using "try-catch" I can set "setHttpError(error)" to error.message.
+// With that we should be setting this error state. Now we can also utilize that and can check (add "ifcheck").
+// STEP 2:
+// 2.1 Add "ifcheck" after "if(isLoading)". We can check if "httpError" is truthy, so if it has a string with some text inside of it. In that case return yet another state. Let's return a section with a text of "httpError" so where I output my error text.
+// 2.2 Add a class "MealsError" to this <section>
+// 2.3 Add appropriate CSS class.
+// 2.4 Add await because "fetchMeals" is acyns function! It also returns a promise. Now, if we throw an error istead of a promise that error will cause that promise to reject. So I can't use "try-catch" to wrap it unless we also await for which we would need to turn our "useEffect" function into an async function, which were not allowed to do.
+// I could still work around that, by putting that into a separete function, which I call thereafter. Need one more function - for error handling, but simply use ".catch()" method.
+// 2.5 Since that returns a promise, I can add ".then()" to handle success cases when the promise fulfills successfully. And add ".catch()" to handle errors coming up inside of the promise
+// 2.6 I wanna catch the error we're getting and move code "setIsLoading(false);setHttpError(error.message)" into catch block
+// ~~ HANDLING THE ERROR ~~
+
+//~~ ADDING A CHECKOUT FORM ~~
+//
+//~~ ADDING A CHECKOUT FORM ~~
